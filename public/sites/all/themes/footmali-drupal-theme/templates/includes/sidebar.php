cac71ec9ed07c69bb937abab42f2671c8a6c8703
Souleymane Coulibaly <coulibaly_souleymane@bah.com>
2019-08-03 21:35:43 -0400

    fix ad display

--- public/sites/all/themes/footmali-drupal-theme/templates/includes/sidebar.php
+++ public/sites/all/themes/footmali-drupal-theme/templates/includes/sidebar.php
@@ -1,6 +1,5 @@
 <div class="sidebar widget-area-11">
-    <?php if(drupal_is_front_page()): ?>
-        <div class="widget kopa-ads-widget">
+<div class="widget kopa-ads-widget">
         <!-- Sidebar -->
         <ins class="adsbygoogle"
             style="display:block"
@@ -8,29 +7,12 @@
             data-ad-slot="5771447117"
             data-ad-format="auto"
             data-full-width-responsive="true"></ins>
-             <script>
+            <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
-             </script>
+            </script>
         </div>
         <!-- widget -->
-     <?php endif; ?>
 
     <?php if ($page['sidebar_second']){ print render($page['sidebar_second']); } ?>
-
-    <?php if(!drupal_is_front_page()): ?>
-        <div class="widget kopa-ads-widget">
-        <!-- Sidebar -->
-        <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-7538390076513661"
-            data-ad-slot="5771447117"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-             <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-             </script>
-        </div>
-        <!-- widget -->
-    <?php endif; ?>
 </div>
 <!-- sidebar -->
